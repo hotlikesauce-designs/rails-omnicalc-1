0e1bbb457a97e4707f4d51e223f80bd272fa0063
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-10-29 20:25:32 +0000

    fixed input/output variables, started payments

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -4,7 +4,8 @@ class CalculationsController < ApplicationController
     render({:template=>"home_templates/homepage"}) #note: don't need to include .html.erb tag
   end
   def square_results
-    @output = (params.fetch("user_number").to_f**2)
+    @input = params.fetch("user_number")
+    @output = (@input.to_f**2)
     render({:template=>"calculations_templates/square_output"}) #note: don't need to include .html.erb tag
   end
   ################################################################
@@ -17,6 +18,14 @@ class CalculationsController < ApplicationController
     @output = (@input.to_f**(0.5))
     render({:template=>"calculations_templates/square_root_output"}) #note: don't need to include .html.erb tag
   end
-  ################
-
+  ################################################################
+  def payment_new
+    #pp params
+    render({:template=>"home_templates/payment"}) #note: don't need to include .html.erb tag
+  end
+  def payment_results
+    @input = params.fetch("user_number")
+    @output = (@input.to_f**(0.5))
+    render({:template=>"calculations_templates/square_root_output"}) #note: don't need to include .html.erb tag
+  end
 end
